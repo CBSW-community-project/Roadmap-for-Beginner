dbaa0d9dbea035538201a58f48693f3cc11363ec
YeongJae <cndudwo1226@gmail.com>
2022-08-27 16:41:34 +0900

    Feat: 추가 프론트엔드 링크[#12]
    프론트엔드 링크 추가

--- src/data/accessibility.js
+++ src/data/accessibility.js
@@ -9,9 +9,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "It is used on the frontend and gives the structure to the webpage.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["W3Schools: Learn HTML", "https://www.w3schools.com/html/html_intro.asp"],
+							["HTML Full Course - Build a Website Tutorial", "https://www.youtube.com/watch?v=pQN-pnXPaVg"]
 						]
 					}
 				},
@@ -20,9 +19,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Semantic element clearly describes its meaning to both the browser and the developer.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["W3Schools: Semantic HTML", "https://www.w3schools.com/html/html5_semantic_elements.asp"],
+							["How To Write Semantic HTML", "https://hackernoon.com/how-to-write-semantic-html-dkq3ulo"]
 						]
 					}
 				},
@@ -31,10 +29,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Client-side form validation helps ensure data submitted matches the requirements set forth in the various form controls.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
-							["", ""]
+							["MDN Web Docs: Client-side form validation", "https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation"],
+							["Learn Forms by web.dev", "https://web.dev/learn/forms/"]
 						]
 					}
 				},
@@ -43,8 +39,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Web accessibility means that websites, tools, and technologies are designed and developed in such a way that people with disabilities can use them easily.",
 						links: [
-							["", ""],
-							["", ""],
+							["Accessibility Tutorial", "https://www.w3schools.com/accessibility/index.php"],
+							["A Complete Guide To Accessible Front-End Components", "https://www.smashingmagazine.com/2021/03/complete-guide-accessible-front-end-components/"]							", ""]
 						]
 					}
 				},
@@ -53,8 +49,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "SEO or Search Engine Optimization is the technique used to optimize your website for better rankings on search engines such as Google, Bing etc.",
 						links: [
-							["", ""],
-							["", ""]
+							["SEO Guide", "https://github.com/seo/guide"],
+							["SEO for Developers", "https://medium.com/welldone-software/seo-for-developers-a-quick-overview-5b5b7ce34679"]
 						]
 					}
 				}
@@ -68,13 +64,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "CSS is the language used to style the frontend of any website. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
-							["", ""],
-							["", ""],
-							["", ""],
-							["", ""],
+							["W3Schools — Learn CSS", "https://www.w3schools.com/css/"],
+							["Learn to Code HTML & CSS", "https://learn.shayhowe.com/html-css/building-your-first-web-page/"]
 						]
 					}
 				},
@@ -83,11 +74,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Float, grid, flexbox, positioning, display and box model are some of the key topics that are used for making layouts.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
-							["", ""],
-							["", ""]
+							["Learn and Practice Flexbox", "https://flexboxfroggy.com/"],
+							["Positioning Types: How Do They Differ?", "https://css-tricks.com/absolute-relative-fixed-positioining-how-do-they-differ/"]
 						]
 					}
 				},
@@ -96,9 +84,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Responsive Web Designing is the technique to make your webpages look good on all screen sizes.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Responsive Web Design", "https://www.w3schools.com/css/css_rwd_intro.asp"],
+							["Learn Responsive Design", "https://web.dev/learn/design/"]
 						]
 					}
 				},
@@ -107,9 +94,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "CSS is notoriously difficult to manage in large, complex, rapidly-iterated systems. A CSS architecture brings reasoning to CSS authoring.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""]
+							["A Look at Some CSS Methodologies", "https://www.webfx.com/blog/web-design/css-methodologies/"]
 						]
 					},
 				},
@@ -118,9 +103,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "CSS Preprocessors are scripting languages that extend the default capabilities of CSS.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["CSS Preprocessors Explained", "https://www.freecodecamp.org/news/css-preprocessors/#:~:text=CSS%20Preprocessors%20compile%20the%20code,preprocessor%20were%20not%20in%20place."],
+							["Why Use Preprocessors?", "https://sherocommerce.com/what-is-a-css-preprocessors-why-use-them/"]
 						]
 					},
 				},
@@ -134,9 +118,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "JavaScript allows you to add interactivity to your pages.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["W3Schools – JavaScript Tutorial", "https://www.w3schools.com/js/"],
+							["The Modern JavaScript Tutorial", "https://javascript.info/"]
 						]
 					}
 				},
@@ -145,9 +128,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "The Document Object Model (DOM) connects web pages to scripts or programming languages.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["What is the DOM?", "https://www.freecodecamp.org/news/what-is-the-dom-document-object-model-meaning-in-javascript/"],
+							["JavaScript DOM", "https://www.javascripttutorial.net/javascript-dom/"]
 						]
 					}
 				},
@@ -156,9 +138,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Ajax is the technique that lets us send and receive the data asynchronously from the servers.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Fetch API MDN Docs", "https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API"],
+							["A Simple Guide to JavaScript Fetch API", "https://www.javascripttutorial.net/javascript-fetch-api/"]
 						]
 					}
 				},
@@ -167,9 +148,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "ECMAScript 2015 or ES2015 is a significant update to the JavaScript programming language.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["ES6 Tutorial", "https://www.javascripttutorial.net/es6/"],
+							["Learn Modern JavaScript in 1 Hour", "https://www.youtube.com/watch?v=NCwa_xi0Uuc"]
 						]
 					}
 				},
@@ -178,9 +158,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "Hoisting allows functions to be safely used in code before they are declared.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["JavaScript Hoisting", "https://developer.mozilla.org/en-US/docs/Glossary/Hoisting"]
 						]
 					}
 				},
@@ -189,9 +167,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "Event bubbling refers to the characteristic that when an event occurs in a specific screen element, the event is transmitted to higher screen elements.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Event Bubbling and Capturing", "https://javascript.info/bubbling-and-capturing"]
 						]
 					}
 				},
@@ -200,9 +176,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "The scope is the current context of execution in which values and expressions are visible or can be referenced.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Scope in JavaScript", "https://developer.mozilla.org/en-US/docs/Glossary/Scope"]
 						]
 					}
 				},
@@ -211,9 +185,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "The prototype is an object that is associated with every functions and objects by default in JavaScript.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Inheritance and Prototype Chain", "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain"]
 						]
 					}
 				},
@@ -222,9 +194,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "Shadow DOM refers to the ability of the browser to include a subtree of DOM elements into the rendering of a document.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["DOM vs Shadow DOM vs Virtual DOM", "https://www.youtube.com/watch?v=7Tok22qxPzQ"]
 						]
 					}
 				},
@@ -233,9 +203,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "WebAssembly is a new type of code that can be run in modern web browsers.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["An introduction to WebAssembly", "https://www.youtube.com/watch?v=3sU557ZKjUs"]
 						]
 					}
 				}
@@ -249,9 +217,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "Npm is a package manager for the JavaScript programming language maintained by npm.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["An Absolute Beginner's Guide to Using npm", "https://nodesource.com/blog/an-absolute-beginners-guide-to-using-npm/"]
 						]
 					}
 				},
@@ -260,9 +226,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "Yarn is a software packaging system developed in 2016 by Facebook for Node.js JavaScript runtime environment.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Modern JavaScript for Dinosaurs", "https://peterxjang.com/blog/modern-javascript-explained-for-dinosaurs.html"]
 						]
 					}
 				},
@@ -276,9 +240,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "Task Runner is a default implementation of a task runner that is provided by AWS Data Pipeline.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Being Productive With Task Runners", "https://www.smashingmagazine.com/2016/06/harness-machines-productive-task-runners/"]
 						]
 					},
 					skills: [
@@ -287,10 +249,7 @@ export const accessibilityCollection = {
 							description: {
 								text: "Npm scripts are the entries in the scripts field of the package.json file.",
 								links: [
-									["", ""],
-									["", ""],
-									["", ""],
-									["", ""]
+									["Introduction to npm scripts", "https://www.geeksforgeeks.org/introduction-to-npm-scripts/"]
 								]
 							},
 						},
@@ -301,9 +260,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "A linter is a tool used to analyze code and discover bugs, syntax errors, stylistic inconsistencies, and suspicious constructs.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["What Is a Linter?", "https://www.testim.io/blog/what-is-a-linter-heres-a-definition-and-quick-start-guide/"]
 						]
 					},
 					skills: [
@@ -312,10 +269,7 @@ export const accessibilityCollection = {
 							description: {
 								text: "Prettier is an opinionated code formatter with support for JavaScript, HTML, CSS, and more.",
 								links: [
-									["", ""],
-									["", ""],
-									["", ""],
-									["", ""]
+									["Prettier Website", "https://prettier.io/"]
 								]
 							},
 						},
@@ -324,10 +278,7 @@ export const accessibilityCollection = {
 							description: {
 								text: "With ESLint you can impose the coding standard using a certain set of standalone rules.",
 								links: [
-									["", ""],
-									["", ""],
-									["", ""],
-									["", ""]
+									["ESLint Official Website", "https://eslint.org/"]
 								]
 							},
 						},
@@ -338,9 +289,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "A module bundler is a tool that takes pieces of JavaScript and their dependencies and bundles them into a single file, usually for use in the browser.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Module Bundlers Explained", "https://www.youtube.com/watch?v=5IG4UmULyoA"],
+							["Let’s learn how module bundlers work", "https://www.freecodecamp.org/news/lets-learn-how-module-bundlers-work-and-then-write-one-ourselves-b2e3fe6c88ae/"]
 						]
 					},
 					skills: [
@@ -349,10 +299,8 @@ export const accessibilityCollection = {
 							description: {
 								text: "Webpack is a module bundler.",
 								links: [
-									["", ""],
-									["", ""],
-									["", ""],
-									["", ""]
+									["Webpack Official Website", "https://webpack.js.org/"],
+									["Webpack Documentation", "https://webpack.js.org/concepts/"]
 								]
 							},
 						},
@@ -361,10 +309,8 @@ export const accessibilityCollection = {
 							description: {
 								text: "Rollup is a module bundler for JavaScript which compiles small pieces of code into something larger and more complex, such as a library or application.",
 								links: [
-									["", ""],
-									["", ""],
-									["", ""],
-									["", ""]
+									["Official Website and Docs", "https://rollupjs.org/"],
+									["How to Set Up JavaScript Bundling Using Rollup", "https://www.youtube.com/watch?v=ICYLOZuFMz8"]
 								]
 							},
 						},
@@ -373,10 +319,8 @@ export const accessibilityCollection = {
 							description: {
 								text: "Parcel is a web application bundler, differentiated by its developer experience.",
 								links: [
-									["", ""],
-									["", ""],
-									["", ""],
-									["", ""]
+									["Official Website and Docs", "https://parceljs.org/plugin-system/bundler/"],
+									["Using Parcel Bundler with React", "https://www.youtube.com/watch?v=hCxvp3_o0gM"]
 								]
 							},
 						},
@@ -392,9 +336,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "React is the most popular front-end JavaScript library for building user interfaces.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["React Website", "https://reactjs.org/"],
+							["The Beginner's Guide to React", "https://egghead.io/courses/the-beginner-s-guide-to-react"]
 						]
 					}
 				},
@@ -403,9 +346,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "Angular is a component based front-end development framework built on TypeScript.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Official - Getting started with Angular", "https://angular.io/start"]
 						]
 					}
 				},
@@ -414,9 +355,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Vue.js is an open-source JavaScript framework for building user interfaces and single-page applications.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Vue.js Website", "https://vuejs.org/"],
+							["Official Getting Started", "https://vuejs.org/v2/guide/"]
 						]
 					}
 				},
@@ -425,9 +365,7 @@ export const accessibilityCollection = {
 					description: {
 						text: "Reactstrap provides prebuilt Bootstrap 4 components that allow a great deal of flexibility and prebuilt validation.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Add Reactstrap Components In ReactJS", "https://www.c-sharpcorner.com/article/reactstrap-in-reactjs/"]
 						]
 					}
 				},
@@ -436,9 +374,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Material-UI is an open-source framework that features React components that implement Google’s Material Design.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Official Website", "https://mui.com/"],
+							["Material UI React Tutorial", "https://www.youtube.com/watch?v=vyJU9efvUtQ"]
 						]
 					}
 				},
@@ -447,9 +384,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Bootstrap is the most popular CSS Framework for developing responsive and mobile-first websites.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Bootstrap Website", "https://getbootstrap.com/"],
+							["Bootstrap Official Documentation", "https://getbootstrap.com/docs/5.2/getting-started/introduction/"]
 						]
 					}
 				},
@@ -463,9 +399,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Next.js is an open-source development framework built on top of Node.js.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Official Website", "https://nextjs.org/"],
+							["The Next.js Handbook — FreeCodeCamp", "https://www.freecodecamp.org/news/the-next-js-handbook/"]
 						]
 					}
 				},
@@ -474,9 +409,8 @@ export const accessibilityCollection = {
 					description: {
 						text: "Gatsby is a React-based open source framework with performance, scalability and security built-in.",
 						links: [
-							["", ""],
-							["", ""],
-							["", ""],
+							["Gatsby Website", "https://www.gatsbyjs.com/"],
+							["Gatsby Tutorial", "https://youtube.com/playlist?list=PL4cUxeGkcC9hw1g77I35ZivVLe8k2nvjB"]
 						]
 					}
 				},
