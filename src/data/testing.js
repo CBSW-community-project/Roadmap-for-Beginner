89caabde172f2803268eb23bebcdf0cfaffd4fa9
Woo_jin <waw1159@gmail.com>
2022-08-29 18:52:04 +0900

    Feat: 블록체인 텍스트 추가[#41]

--- src/data/testing.js
+++ src/data/testing.js
@@ -4,20 +4,98 @@ export const testingCollection = {
 		{
 			skills: [
 				{
-					name: "Testing Methodologies",
+					name: "Knowledge",
 					description: {
-						text: "Learn about various software testing methodologies and why it is important to test your code.",
+						text: "Learn about the overall understanding and application of blockchain.",
+						links: [
+							["Wikipedia - Software testing", "https://en.wikipedia.org/wiki/Software_testing"],
+							["Software Testing Methodologies", "https://smartbear.com/learn/automated-testing/software-testing-methodologies/"],
+							["Learn The Methods & Tools", "https://www.inflectra.com/ideas/topic/testing-methodologies.aspx"],
+						],
+					},
+				},
+				{
+					name: "Structure",
+					description: {
+						text: "Learn about the blockchain structure.",
 						links: [
 							["Wikipedia - Software testing", "https://en.wikipedia.org/wiki/Software_testing"],
 							["Software Testing Methodologies", "https://smartbear.com/learn/automated-testing/software-testing-methodologies/"],
 							["Learn The Methods & Tools", "https://www.inflectra.com/ideas/topic/testing-methodologies.aspx"],
 						]
 					},
+				},
+				{
+					name: "Operation",
+					description: {
+						text: "Learn about the basic operation of blockchain.",
+						links: [
+							["Wikipedia - Software testing", "https://en.wikipedia.org/wiki/Software_testing"],
+							["Software Testing Methodologies", "https://smartbear.com/learn/automated-testing/software-testing-methodologies/"],
+							["Learn The Methods & Tools", "https://www.inflectra.com/ideas/topic/testing-methodologies.aspx"],
+						]
+					},
+				},
+				{
+					name: "Forking",
+					description: {
+						text: "A fork happens whenever a community makes a change to the blockchain’s protocol, or basic set of rules.",
+						links: [
+							["Wikipedia - Software testing", "https://en.wikipedia.org/wiki/Software_testing"],
+							["Software Testing Methodologies", "https://smartbear.com/learn/automated-testing/software-testing-methodologies/"],
+							["Learn The Methods & Tools", "https://www.inflectra.com/ideas/topic/testing-methodologies.aspx"],
+						]
+					},
+				},
+				{
+					name: "Cryptocurrencies",
+					description: {
+						text: "A cryptocurrency is a digital currency designed to work as a medium of exchange through a blockchain, which is not reliant on any central authority, such as a government or bank, to uphold or maintain it.",
+						links: [
+							["Wikipedia - Software testing", "https://en.wikipedia.org/wiki/Software_testing"],
+							["Software Testing Methodologies", "https://smartbear.com/learn/automated-testing/software-testing-methodologies/"],
+							["Learn The Methods & Tools", "https://www.inflectra.com/ideas/topic/testing-methodologies.aspx"],
+						]
+					},
+				},
+				{
+					name: "Mining and Icentive Models",
+					description: {
+						text: "Mining is the process of adding transaction details to the Blockchain, like sender address, hash value, etc.",
+						links: [
+							["Wikipedia - Software testing", "https://en.wikipedia.org/wiki/Software_testing"],
+							["Software Testing Methodologies", "https://smartbear.com/learn/automated-testing/software-testing-methodologies/"],
+							["Learn The Methods & Tools", "https://www.inflectra.com/ideas/topic/testing-methodologies.aspx"],
+						]
+					},
+				},
+				{
+					name: "Cryptography",
+					description: {
+						text: "Cryptography is the practice and study of techniques for secure communication in the presence of adversarial behavior.",
+						links: [
+							["Wikipedia - Software testing", "https://en.wikipedia.org/wiki/Software_testing"],
+							["Software Testing Methodologies", "https://smartbear.com/learn/automated-testing/software-testing-methodologies/"],
+							["Learn The Methods & Tools", "https://www.inflectra.com/ideas/topic/testing-methodologies.aspx"],
+						]
+					},
+				},
+				//
+				{
+					name: "EVM Based",
+					description: {
+						text: "The Ethereum Virtual Machine (EVM) is a dedicated software virtual stack that executes smart contract bytecode and is integrated into each Ethereum node.",
+						links: [
+							["dev.to - Continuous Integration Explained", "https://dev.to/markoa/continuous-integration-explained-59f9"],
+							["Continuous Integration Tutorial", "https://www.tutorialspoint.com/continuous_integration/index.htm"],
+							["Wikipedia - Continuous integration", "https://en.wikipedia.org/wiki/Continuous_integration"],
+						]
+					},
 					skills: [
 						{
-							name: "Unit Testing",
+							name: "Ethereum",
 							description: {
-								text: "Learn about unit testing and how it can be used to test individual units of code.",
+								text: "The Ethereum native token is the Ether (ETH), and it’s used to fuel operations on the blockchain.",
 								links: [
 									["Unit Testing Tutorial", "https://www.guru99.com/unit-testing-guide.html"],
 									["Wikipedia - Unit testing", "https://en.wikipedia.org/wiki/Unit_testing"],
@@ -27,117 +105,23 @@ export const testingCollection = {
 							},
 						},
 						{
-							name: "Integration Testing",
+							name: "Polygon",
 							description: {
-								text: "Learn about integration testing and how it can be used to test modules as a group.",
+								text: "Polygon is a scaling solution that aims to provide multiple tools to improve the speed and reduce the cost and complexities of transactions on the Ethereum blockchain.",
 								links: [
 									["Integration Testing Tutorial", "https://www.guru99.com/integration-testing.html"],
 									["What is Integration Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/integration_testing.htm"],
 									["Integration Testing", "http://softwaretestingfundamentals.com/integration-testing/"],
 								]
 							},
-						},
-						{
-							name: "System Testing",
-							description: {
-								text: "Learn about system testing and how it can be used to test the fully integrated software product.",
-								links: [
-									["System Testing Tutorial", "https://www.guru99.com/system-testing.html"],
-									["What is System Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/system_testing.htm"],
-									["System Testing", "http://softwaretestingfundamentals.com/system-testing/"],
-								]
-							},
-						},
-						{
-							name: "Acceptance Testing",
-							description: {
-								text: "Learn about acceptance testing and how it can be used to test the software for acceptability",
-								links: [
-									["What is User Acceptance Testing?", "https://www.guru99.com/user-acceptance-testing.html"],
-									["Acceptance Testing", "http://softwaretestingfundamentals.com/acceptance-testing/"],
-									["What is Acceptance Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/acceptance_testing.htm"],
-								]
-							},
-						},
-						{
-							name: "Smoke Testing",
-							description: {
-								text: "Learn about smoke testing and how it can be used to ensure that critical functionalities of the program are working fine.",
-								links: [
-									["Sanity Testing Vs Smoke Testing", "https://www.guru99.com/smoke-sanity-testing.html"],
-									["Smoke Testing", "http://softwaretestingfundamentals.com/smoke-testing/"],
-								]
-							},
-						},
-						{
-							name: "Performance Testing",
-							description: {
-								text: "Learn about performance testing and how it can be used to determine the responsiveness and stability of a program.",
-								links: [
-									["Performance Testing Tutorial", "https://www.guru99.com/performance-testing.html"],
-									["Performance Testing", "http://softwaretestingfundamentals.com/performance-testing/"],
-									["Wikipedia - Software performance testing", "https://en.wikipedia.org/wiki/Software_performance_testing"],
-								]
-							},
-						},
-						{
-							name: "Usability Testing",
-							description: {
-								text: "Learn about usability testing and how it can be used to determine if the system is easily usable for the end-user.",
-								links: [
-									["What is Usability Testing?", "https://www.guru99.com/usability-testing.html"],
-									["Usability Testing", "http://softwaretestingfundamentals.com/usability-testing/"],
-									["Wikipedia - Usability testing", "https://en.wikipedia.org/wiki/Usability_testing"],
-								]
-							},
-						},
-						{
-							name: "White Box Testing",
-							description: {
-								text: "Learn about white box testing and how it can be used to test the software's internal structure, design, and coding.",
-								links: [
-									["What is White Box Testing?", "https://www.guru99.com/white-box-testing.html"],
-									["White Box Testing", "http://softwaretestingfundamentals.com/white-box-testing/"],
-								]
-							},
-						},
-						{
-							name: "Black Box Testing",
-							description: {
-								text: "Learn about black box testing and how a test can be carried out without looking at the internal code.",
-								links: [
-									["What is Black Box Testing?", "https://www.guru99.com/black-box-testing.html"],
-									["Black Box Testing", "http://softwaretestingfundamentals.com/black-box-testing/"],
-								]
-							},
-						},
-						{
-							name: "Automated Testing",
-							description: {
-								text: "Learn about automated testing and when it should be applied.",
-								links: [
-									["What is Automated Testing?", "https://smartbear.com/learn/automated-testing/what-is-automated-testing/"],
-									["Automated Testing Tutorial", "https://www.guru99.com/automation-testing.html"],
-								]
-							},
-						},
-						{
-							name: "Manual Testing",
-							description: {
-								text: "Learn about manual testing and when it should be applied.",
-								links: [
-									["Manual Testing Tutorial", "https://www.guru99.com/manual-testing.html"],
-									["Wikipedia - Manual Testing", "https://en.wikipedia.org/wiki/Manual_testing"],
-									["What is Manual Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/manual_testing.htm"],
-								]
-							},
-						},
-					]
+						}
+					],
 				},
+				//
 				{
-					name: "Continuous integration",
+					name: "Smart Contracts",
 					description: {
-						text: "Learn about continuous integration and it can enable iterative software development.",
+						text: "Learn about Smart Contracts and their uses.",
 						links: [
 							["dev.to - Continuous Integration Explained", "https://dev.to/markoa/continuous-integration-explained-59f9"],
 							["Continuous Integration Tutorial", "https://www.tutorialspoint.com/continuous_integration/index.htm"],
@@ -146,9 +130,9 @@ export const testingCollection = {
 					},
 				},
 				{
-					name: "A/B Testing",
+					name: "Smart Framework",
 					description: {
-						text: "Learn about A/B testing and how it can be used to optimize your web app.",
+						text: "Software frameworks include many of the needed features or provide easy plugin systems to pick the tools you desire.",
 						links: [
 							["dev.to - A/B testing: introduction", "https://dev.to/ice_lenor/ab-testing-introduction-3jpd"],
 							["dev.to - When Not To A/B Test", "https://dev.to/ice_lenor/when-not-to-ab-test-4cc7"],
@@ -157,10 +141,11 @@ export const testingCollection = {
 						]
 					},
 				},
+				//
 				{
-					name: "Test Runners",
+					name: "Dapp",
 					description: {
-						text: "Learn about test runners and how they can help running tests.",
+						text: "A decentralised application (dApp) is an application that can operate autonomously, through the use of smart contracts that run on a blockchain.",
 						links: [
 							["Classifications of Testing Software", "https://amzotti.github.io/testing/2015/03/16/what-is-the-difference-between-a-test-runner-testing-framework-assertion-library-and-a-testing-plugin/"],
 							["Udacity - Javascript Testing", "https://www.udacity.com/course/javascript-testing--ud549"],
@@ -169,9 +154,9 @@ export const testingCollection = {
 					},
 					skills: [
 						{
-							name: "Karma",
+							name: "NaaS",
 							description: {
-								text: "Learn about the Karma test runner.",
+								text: "Learn about the NaaS.",
 								links: [
 									["Karma", "https://karma-runner.github.io/latest/index.html"],
 									["Getting started with Karma and Mocha", "https://medium.com/@jazcodes/getting-started-with-karma-and-mocha-for-automated-browser-tests-5ebb6cd02edf"],
@@ -179,59 +164,54 @@ export const testingCollection = {
 							}
 						},
 						{
-							name: "Mocha",
+							name: "Supporting Languages",
 							description: {
-								text: "Learn about the Mocha testing framework.",
+								text: "All the surrounding services that support those smart contracts (frontend, monitoring, etc.) are often written in other languages.",
 								links: [
 									["Mocha", "https://mochajs.org/"],
 									["A quick and complete guide to Mocha testing", "https://blog.logrocket.com/a-quick-and-complete-guide-to-mocha-testing-d0e0ea09f09d/"],
 									["How to test JavaScript with Mocha", "https://codeburst.io/how-to-test-javascript-with-mocha-the-basics-80132324752e"],
 								]
 							}
-						},
-						{
-							name: "Jasmine",
-							description: {
-								text: "Learn about the Jasmine testing frameworks.",
-								links: [
-									["Jasmine", "https://jasmine.github.io/"],
-									["How to set up a testing environment with Jasmine and karma", "https://medium.com/bb-tutorials-and-thoughts/javascript-how-to-set-up-a-testing-environment-with-jasmine-and-karma-58591dd39734"],
-								]
-							}
-						},
-						{
-							name: "Cypress",
-							description: {
-								text: "Learn how to use Cypress for end-to-end-testing.",
-								links: [
-									["Cypress", "https://www.cypress.io/"],
-									["End-to-End Testing with Cypress", "https://webdevstudios.com/2018/11/13/end-to-end-testing-with-cypress-io/"],
-								]
-							}
-						},
-						{
-							name: "Ava",
-							description: {
-								text: "Learn about the Ava test runner.",
-								links: [
-									["Github - Ava", "https://github.com/avajs/ava"],
-									["A Fresh Take On JavaScript Testing", "https://pusher.com/sessions/meetup/the-js-roundabout/ava-test-runner-a-fresh-take-on-javascript-testing-and-growing-an-open-source-project"],
-									["Freecodecamp - How you can test your Node.js applications", "https://www.freecodecamp.org/news/testing-your-nodejs-applications-with-ava-js-99e806a226a7/"],
-								]
-							},
 						}
 					]
 				},
+				//
 				{
-					name: "Best Practices",
+					name: "Oracle",
 					description: {
-						text: "Learn about the best practices when it comes to testing.",
+						text: "A blockchain oracle is a third-party service that connects smart contracts with the outside world.",
 						links: [
-							["Javascript Testing Best Practices", "https://github.com/goldbergyoni/javascript-testing-best-practices"],
-							["What are the best practices in software Testing?", "https://www.quora.com/What-are-the-best-practices-in-software-Testing"],
-							["7 Software Testing Best Practices", "https://www.qasymphony.com/blog/7-software-testing-best-practices/"],
+							["dev.to - Continuous Integration Explained", "https://dev.to/markoa/continuous-integration-explained-59f9"],
+							["Continuous Integration Tutorial", "https://www.tutorialspoint.com/continuous_integration/index.htm"],
+							["Wikipedia - Continuous integration", "https://en.wikipedia.org/wiki/Continuous_integration"],
 						]
-					}
+					},
+					skills: [
+						{
+							name: "Chainlink",
+							description: {
+								text: "Chainlink is a decentralized network of oracles that enables smart contracts to securely interact with real-world data and services that exist outside of blockchain networks.",
+								links: [
+									["Unit Testing Tutorial", "https://www.guru99.com/unit-testing-guide.html"],
+									["Wikipedia - Unit testing", "https://en.wikipedia.org/wiki/Unit_testing"],
+									["What is Unit Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/unit_testing.htm"],
+									["The Arrange, Act and Assert (AAA) Pattern", "https://medium.com/@pjbgf/title-testing-code-ocd-and-the-aaa-pattern-df453975ab80"],
+								]
+							},
+						},
+						{
+							name: "Oracle Networks",
+							description: {
+								text: "Decentralized oracle networks provide an increased level of security and fairness to smart contracts.",
+								links: [
+									["Integration Testing Tutorial", "https://www.guru99.com/integration-testing.html"],
+									["What is Integration Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/integration_testing.htm"],
+									["Integration Testing", "http://softwaretestingfundamentals.com/integration-testing/"],
+								]
+							}
+						}
+					]
 				}
 			]
 		}
